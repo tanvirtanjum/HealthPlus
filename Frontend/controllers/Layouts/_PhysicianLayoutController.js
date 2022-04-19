b06d2187e83a348f6015b0d7090f0e2d973f7dba
Tanvir Tanjum Shourav <tanjumtanvir@gmail.com>
2022-04-19 22:43:44 +0600

    ./

--- Frontend/controllers/Layouts/_PhysicianLayoutController.js
+++ Frontend/controllers/Layouts/_PhysicianLayoutController.js
@@ -2,15 +2,15 @@ $(document).ready(function () {
     // var base_URL = "http://127.0.0.1:5500";
     // var api_base_URL = "http://localhost:3000";
 
-    $('#homeTab').attr("href", base_URL+"/views/Public/Home.html");
+    $('#dashTab').attr("href", base_URL+"/views/Physician/Dashboard.html");
     $('#aboutTab').attr("href", base_URL+"/views/Public/About_Us.html");
     $('#signinTab').attr("href", base_URL+"/views/Public/SignIn.html");
 
     function activeSection()
     {
-        if(window.location.href == base_URL+"/views/Public/Home.html")
+        if(window.location.href == base_URL+"/views/Physician/Dashboard.html")
         {
-            $('#homeTab').addClass("active");
+            $('#dashTab').addClass("active");
             $('#aboutTab').removeClass("active");
             $('#signinTab').removeClass("active");
         }
---
+++ Frontend/controllers/Physician/DashboardController.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    // var base_URL = "http://127.0.0.1:5500";
+    // var api_base_URL = "http://localhost:3000";
+
+    $('#topLayout').load("../Layouts/_PhysicianLayout.html");
+    $('#bottomLayout').load("../Layouts/_BottomLayout.html");
+        
+    var redirect = function(role) {
+        if(role == null)
+        {
+            window.location.href = base_URL+"/views/Public/Home.html";
+        }
+        if(role == 1)
+        {
+            window.location.href = base_URL+"/views/Admin/Dashboard.html";
+        }
+        if(role == 2)
+        {
+            window.location.href = base_URL+"/views/Physician/Dashboard.html";
+        }     
+    }
+    
+    var checkLocalStorage = function() {
+        var decryptLoginInfo = CryptoJS.AES.decrypt(localStorage.loginInfo, '333');
+        decryptLoginInfo = decryptLoginInfo.toString(CryptoJS.enc.Utf8);
+        decryptLoginInfo = JSON.parse(decryptLoginInfo);
+
+        if(decryptLoginInfo.status_id == 2) 
+        {
+
+        }
+        else
+        {   
+            redirect(decryptLoginInfo.status_id);
+        }
+    }
+
+    if(localStorage.getItem('loginInfo') == null)
+    {
+        redirect(null);
+    }
+    else
+    {
+        checkLocalStorage();
+    }
+
+});
